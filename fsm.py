d2315b2e141cab3ea8d46cfb0cf11bedd4bd28df
yuchun <slnv126@gmail.com>
2019-11-29 11:09:21 +0800

    Test sample code

--- fsm.py
+++ fsm.py
@@ -1,4 +1,4 @@
-from transitions.extensions import GraphMachine
+"""from transitions.extensions import GraphMachine
 from linebot.models import *
 from utils import send_text_message, send_button_message
 
@@ -16,8 +16,8 @@ class TocMachine(GraphMachine):
     def is_going_to_queen(self, event):
         text = event.message.text
         return text.lower() == "單戀中，求紅線" or text.lower() == "有對象，希望感情加溫"
-        """
-        return text.lower() == "go to state2"""
+        
+        return text.lower() == "go to state2"
     def is_going_to_war(self, event):
         text = event.message.text
         return text.lower() == '想砍掉爛桃花'
@@ -101,3 +101,40 @@ class TocMachine(GraphMachine):
     
     def on_exit_cing(self):
         print("Leaving cing")
+        """
+from transitions.extensions import GraphMachine
+
+from utils import send_text_message
+
+
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(model=self, **machine_configs)
+
+    def is_going_to_state1(self, event):
+        text = event.message.text
+        return text.lower() == "go to state1"
+
+    def is_going_to_state2(self, event):
+        text = event.message.text
+        return text.lower() == "go to state2"
+
+    def on_enter_state1(self, event):
+        print("I'm entering state1")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state1")
+        self.go_back()
+
+    def on_exit_state1(self):
+        print("Leaving state1")
+
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state2")
+        self.go_back()
+
+    def on_exit_state2(self):
+        print("Leaving state2")
