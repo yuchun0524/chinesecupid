bb1a2780765907e773af643bd30e465cd9c8a713
yuchun <slnv126@gmail.com>
2019-12-09 18:46:50 +0800

    Add the function of reply sticker

--- utils.py
+++ utils.py
@@ -3,9 +3,7 @@ import os
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage
 
-
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
